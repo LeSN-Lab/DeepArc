cc0d7f651ffa9e06aade9328ebabc913e30d4518
Serendipity-Song <thdalwh3867@gmail.com>
2023-12-27 16:26:50 +0900

    jax porting-effective_cka, update_state

--- effective_CKA2jax.py
+++ effective_CKA2jax.py
@@ -11,15 +11,16 @@ def generate_gram_matrix(x):
     gram = gram.at[jnp.diag_indices(n)].set(0)
     means = jnp.sum(gram, axis=0) / (n - 2)
     means -= jnp.sum(means) / (2 * (n - 1))
-    gram -= means[1:0]
-    gram -= means[:-1]
+    gram -= means[:, None]
+    gram -= means[None, :]
+    gram = gram.at[jnp.diag_indices(n)].set(0)
     return gram.reshape(-1)
 
 
 # update_state in jax
-
-
-
-
+def update_state(hsic_accumulator, activations):
+    layer_grams = jnp.stack([generate_gram_matrix(x) for x in activations])
+    hsic_accumulator += jnp.dot(layer_grams, layer_grams.T)
+    return hsic_accumulator
 
 # result in jax
